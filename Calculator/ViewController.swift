88a06c75b9dd05344100de13bb36c82e4b4b91c6
Alina Zaitseva <Alina_Zaitseva@epam.com>
2018-04-04 14:40:20 +0300

    create basis structure

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,7 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
-
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+            userIsTheMiddleOfTyping = true
+        }
+    }
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    @IBAction func performOperator(_ sender: UIButton) {
+        userIsTheMiddleOfTyping = false
+        if let mathematiclaSymbol = sender.currentTitle {
+            switch mathematiclaSymbol {
+            case "∏":
+                displayValue = Double.pi
+            case "√":
+                displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+        }
+    }
+    
+    
+    
 }
 
