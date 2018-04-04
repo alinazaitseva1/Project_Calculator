c090ef90a2243a024104824d4599444d9796d55e
Alina Zaitseva <Alina_Zaitseva@epam.com>
2018-04-04 11:55:09 +0300

    initial commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Alina Zaitseva on 4/4/18.
+//  Copyright © 2018 Alina Zaitseva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+
+}
+
